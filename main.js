5502f29b0a5835ad825dc6d98333e50c0c038ea3
Mykyta Zviahin <mykytazviahin@yahoo.com>
2020-08-12 17:33:26 +0300

    Started the server

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-'use strict';
-
-console.log('Start');
-setTimeout(() => {
-
-  console.log('Stop');
-
-}, 2000);
+const http = require( id = 'http');
+const pid = process.pid;
+http
+    .createServer( requestListener = (req,res) => {
+      res.end(chunk = 'Hello from Node.js!');
+    }).listen( port = 8800, listeningListener = () => {
+      console.log('Server started.',pid);
+});
