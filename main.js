91c16259b9bb27439ed53e5b77adf9c92da0fb43
Mykyta Zviahin <mykytazviahin@yahoo.com>
2020-08-13 11:35:19 +0300

    Config modification and cluster added

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const http = require( id = 'http');
 const pid = process.pid;
 http
     .createServer( requestListener = (req,res) => {
+      for (let i = 0; i < 1e7; i++) {};
       res.end(chunk = 'Hello from Node.js!');
     }).listen( port = 8800, listeningListener = () => {
       console.log('Server started.',pid);
