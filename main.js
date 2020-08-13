2b2ede6fd06bbc2d4719813def2da4902b773c7d
Mykyta Zviahin <mykytazviahin@yahoo.com>
2020-08-13 15:57:09 +0300

    Added clusterization, and some reliability in file cluster.js

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
-const http = require( id = 'http');
+const http = require('http');
 const pid = process.pid;
-http
-    .createServer( requestListener = (req,res) => {
-      for (let i = 0; i < 1e7; i++) {};
-      res.end(chunk = 'Hello from Node.js!');
-    }).listen( port = 8800, listeningListener = () => {
-      console.log('Server started.',pid);
-});
+const port = 8800;
+const chunk = 'Hello from Node.js!';
+
+const requestListener = (req,res) => {
+  for (let i = 0; i < 1e7; i++);
+  res.end(chunk);
+}
+
+const listeningListener = () => {
+  console.log('Server started. Pid: ',pid);
+}
+
+http.createServer(requestListener).listen( port, listeningListener );
